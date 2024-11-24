fd24766f990e9a9f28cec5aa7076a2e90b6a67d6
Zen-Lex <jbertink@gmail.com>
2024-11-24 02:06:42 +0100

    see i pushed

--- packages/test_package/src/pilot_node.py
+++ packages/test_package/src/pilot_node.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 import os
+import rospy
+from duckietown.dtros import DTROS, NodeType
+from duckietown_msgs.msg import WheelsCmdStamped
 
 class PilotNode(DTROS):
 
     def __init__(self, node_name):
         super().__init__(node_name=node_name, node_type=NodeType.CONTROL)
 
-        self._vehicle_name = os.environ.get('VehicleName')
-        self._topic = f"/{self._vehicle_name}/wheels_driver_node/wheels_cmd"
+        vehicle_name = os.environ['VEHICLE_NAME']
+        topic = f"/{vehicle_name}/wheels_driver_node/wheels_cmd"
         self._publisher = rospy.Publisher(topic, WheelsCmdStamped, queue_size=1)
 
     def run(self):
@@ -21,3 +24,8 @@ class PilotNode(DTROS):
     def on_shutdown(self):
         stop = WheelsCmdStamped(vel_left=0.0, vel_right=0.0)
         self._publisher.publish(stop)
+
+if __name__ == '__main__':
+    node = PilotNode(node_name='pilot_node')
+    node.run()
+    rospy.spin()
