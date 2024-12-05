1bd52e7254f9bb0f4150f36b4d6e8be9b312b744
Andrea Pellegrin <apple@andreapellegrin.it>
2024-12-05 11:24:41 +0100

    Added Dumb Pilot node that follow the 3 instructions: straight, left and right using the calibration.

--- packages/test_package/src/smart_pilot_node.py
+++ packages/test_package/src/smart_pilot_node.py
@@ -4,6 +4,7 @@ import rospy
 from duckietown.dtros import DTROS, NodeType
 from duckietown_msgs.msg import WheelsCmdStamped
 
+import math
 
 class PilotNode(DTROS):
 
@@ -19,12 +20,41 @@ class PilotNode(DTROS):
         self._publisher.publish(message)
         rospy.sleep(2)
 
-    def turnLeft(self):
-        message = WheelsCmdStamped(vel_left=1.0, vel_right=1.0)
-        self._publisher.publish(message)
-        rospy.sleep(2)
+    # WORK AREA - START
 
-    def turnRight(self):
+    def getLeftWheelVelocity(self, x):
+        v = 1
+        d = 10
+        r = 18
+
+        startTime = 1.5
+        endTime = 4.5
+
+        if (0 <= x < startTime):
+            return v
+        elif (startTime <= x < endTime):
+            return v * ((d/(2 * r)) * math.sin(math.pi * (x - startTime)/(endTime - startTime)) + 1.0)
+        else:
+            return v
+
+    def getRightWheelVelocity(self, x):
+        startTime = 1.5
+        endTime = 4.5
+
+        if (0 <= x < startTime):
+            return self.getLeftWheelVelocity(x)
+        elif (startTime <= x < endTime):
+            return 2-self.getLeftWheelVelocity(x)
+        else:
+            return self.getLeftWheelVelocity(x)
+
+    def turnRight(self, x):
+        message = WheelsCmdStamped(vel_left=self.getLeftWheelVelocity(x), vel_right=self.getRightWheelVelocity(x))
+        self._publisher.publish(message)
+
+    # WORK AREA - END
+
+    def turnLeft(self):
         message = WheelsCmdStamped(vel_left=1.0, vel_right=1.0)
         self._publisher.publish(message)
         rospy.sleep(2)
@@ -35,13 +65,15 @@ class PilotNode(DTROS):
         rospy.sleep(2)
 
     def run(self):
-        while not rospy.is_shutdown():
-            self.goStraight()
-            self.stay()
-            self.goStraight()
-            self.stay()
+        rate = rospy.Rate(10)
+        start_time = rospy.Time.now()
 
-            rospy.sleep(10)
+        while not rospy.is_shutdown():
+            current_time = rospy.Time.now()  # Current ROS time
+            elapsed_time = (current_time - start_time).to_sec()
+            self.turnRight(elapsed_time)
+
+            rate.sleep()
 
     def on_shutdown(self):
         stop = WheelsCmdStamped(vel_left=0.0, vel_right=0.0)
