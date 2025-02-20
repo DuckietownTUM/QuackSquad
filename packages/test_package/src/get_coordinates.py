2b78dd8ee36d74835fcd8b2b25fa259f386177e0
Zen-Lex <jbertink@gmail.com>
2025-02-20 22:35:37 +0100

    update odometry to be more precise

--- packages/test_package/src/get_coordinates.py
+++ packages/test_package/src/get_coordinates.py
@@ -6,41 +6,33 @@ import math
 from duckietown.dtros import DTROS, NodeType
 from duckietown_msgs.msg import WheelsCmdStamped, WheelEncoderStamped
 
+TICKS_PER_REVOLUTION = 135
 STRAIGHT_THRESHOLD = 0.05 # in rad
 MM_PER_TICK = 1.55
-BASE_LENGTH = 68     # in mm
+BASE_LENGTH = 100     	# in mm
+WHEEL_RADIUS = 31.8 	# in mm
 
-class KinematicsNode(DTROS):
+class OdometryNode(DTROS):
 
 	def __init__(self, node_name):
 		# initialize the DTROS parent class
-		super(KinematicsNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
+		super(OdometryNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
 
 		# static parameters
 		vehicle_name = os.getenv("VEHICLE_NAME", "duckie") # 'duckie' as default, useful when lauching from outside the container
 		left_encoder_topic = f"/{vehicle_name}/left_wheel_encoder_node/tick"
 		right_encoder_topic = f"/{vehicle_name}/right_wheel_encoder_node/tick"
 
-		self.flag_first = True # Flag to skip first loop
+		# Previously recorded encoder tick's values
+		self.prev_ticks_left = None
+		self.prev_ticks_right = None
 
-		# Current encoder tick counters
-		self.ticks_left = None
-		self.ticks_right = None
-
-		# New encoder tick values
+		# New encoder tick's values
 		self.new_ticks_left = None
 		self.new_ticks_right = None
 
-		# Last timestamp values
-		self.last_timestamp_left = None
-		self.last_timestamp_right = None
-
-		# New timestamp values
-		self.new_timestamp_left = None
-		self.new_timestamp_right = None
-
 		# Robot position and orientation
-		self.pos = [0.0, 0.0] # [x, y] (in mm)
+		self.pos = {"x":0.0, "y":0.0} # [x, y] (in mm)
 		self.angle = 0.0  # in radians
 
 		# Subscribers
@@ -52,79 +44,65 @@ class KinematicsNode(DTROS):
 		rospy.loginfo_once(f"Left encoder resolution: {data.resolution}")
 		rospy.loginfo_once(f"Left encoder type: {data.type}")
 
+		if self.prev_ticks_left is None:
+			self.prev_ticks_left = data.data
+
 		# store data value
 		self.new_ticks_left = data.data
-		self.new_timestamp_left = data.header.stamp
 
 	def callback_right(self, data):
 		# log general information once at the beginning
 		rospy.loginfo_once(f"Right encoder resolution: {data.resolution}")
 		rospy.loginfo_once(f"Right encoder type: {data.type}")
 
+		if self.prev_ticks_right is None:
+			self.prev_ticks_right = data.data
+			
 		# store data value
 		self.new_ticks_right = data.data
-		self.new_timestamp_right = data.header.stamp
 
 	def run(self):
-		# publish received tick messages every 0.05 second (20 Hz)
-		rate = rospy.Rate(20)
+		# publish received tick messages every 0.1 second (10 Hz)
+		rate = rospy.Rate(10)
 
 		while not rospy.is_shutdown():
-			if self.new_ticks_left is None or self.new_ticks_right is None:
-				rate.sleep()
-				continue
-
-			if self.flag_first:
-				self.ticks_left = self.new_ticks_left
-				self.ticks_right = self.new_ticks_right
-				self.last_timestamp_left = self.new_timestamp_left
-				self.last_timestamp_right = self.new_timestamp_right
-				self.flag_first = False
-				rate.sleep()
-				continue  # Skip first loop to avoid incorrect deltas
-
-			# Compute the delta times
-			delta_time_left = (self.new_timestamp_left - self.last_timestamp_left).to_sec()
-			delta_time_right = (self.new_timestamp_right - self.last_timestamp_right).to_sec()
-			avg_delta_time = (delta_time_left + delta_time_right) / 2
-
-			# Compute the linear distances traveled by the wheels
-			vel_left = (self.new_ticks_left - self.ticks_left)*MM_PER_TICK / delta_time_left 
-			vel_right = (self.new_ticks_right - self.ticks_right)*MM_PER_TICK / delta_time_right
-
-			# Update the ticks values
-			self.ticks_left = self.new_ticks_left
-			self.ticks_right = self.new_ticks_right
-
-			# Compute linear and angular velocities
-			avg_vel = (vel_left + vel_right) / 2
-			angular_vel = (vel_right - vel_left) / BASE_LENGTH # omega (ω)
-
-			if abs(angular_vel) < STRAIGHT_THRESHOLD:  # Going straight
-				self.pos[0] += avg_vel * avg_delta_time * math.cos(self.angle + angular_vel * avg_delta_time)
-				self.pos[1] += avg_vel * avg_delta_time * math.sin(self.angle + angular_vel * avg_delta_time)
-			else:  # Turning
-				# Compute the distance to the instantaneous center of rotation (ICR)
-				dist_to_icr = avg_vel / angular_vel # from formula R = B/2 * (Vr + Vl) / (Vr - Vl)
-
-				self.pos[0] += dist_to_icr * (math.sin(self.angle + angular_vel*avg_delta_time) - math.sin(self.angle))
-				self.pos[1] += dist_to_icr * (-math.cos(self.angle + angular_vel*avg_delta_time) + math.cos(self.angle))
-
-			# Update after computing new position
-			self.angle += angular_vel * avg_delta_time
-
-			# log information about the position
-			msg = f"{self.pos[0]//600}|{self.pos[1]//600}(X:{self.pos[0]/10:.2f}/Y:{self.pos[1]/10:.2f}) | Angle:{self.angle*180/math.pi:.4f}"
-			rospy.loginfo(msg)
-
+			if not (self.new_ticks_left is None or self.new_ticks_right is None):
+				self.compute_pos()
+			
 			rate.sleep()
 
+	def compute_pos(self):
+		# Compute the linear distances traveled by the wheels
+		delta_tick_left = self.new_ticks_left - self.prev_ticks_left
+		delta_tick_right = self.new_ticks_right - self.prev_ticks_right
 
-	# def on_shutdown(self):
+		dist_left = 2 * math.pi * WHEEL_RADIUS * delta_tick_left / TICKS_PER_REVOLUTION
+		dist_right = 2 * math.pi * WHEEL_RADIUS * delta_tick_right / TICKS_PER_REVOLUTION
+		avg_dist = (dist_left + dist_right) / 2
+		delta_angle = (dist_right - dist_left) / BASE_LENGTH # theta
+
+		# Update after computing new position
+		self.angle += delta_angle
+
+		self.pos.x += avg_dist * math.cos(self.angle)
+		self.pos.y += avg_dist * math.sin(self.angle )
+
+		# Update the ticks values
+		self.ticks_left = self.new_ticks_left
+		self.ticks_right = self.new_ticks_right
+
+		# log information about the position
+		msg = f"{self.pos[0]//600}|{self.pos[1]//600}(X:{self.pos[0]/10:.2f}/Y:{self.pos[1]/10:.2f}) | Angle:{self.angle*180/math.pi:.4f}"
+		rospy.loginfo(msg)
+
+
+	def on_shutdown(self):
 		# store information before shutdown (maybe, we'll see)
+		pass
 
 if __name__ == '__main__':
 	# create the node
-	node = KinematicsNode(node_name='kinematics_node')
+	node = OdometryNode(node_name='odometry_node')
 	# run node
 	node.run()
+	rospy.spin()
