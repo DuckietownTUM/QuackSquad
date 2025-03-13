f8f8eaeda785c86541095b0888dbccf7254fa352
andreapellegrin <andreapellegrin@hotmail.it>
2025-03-13 13:12:30 +0100

    Launcher deadreckoning fix

--- dt-core/launchers/test-deadreckoning.sh
+++ dt-core/launchers/test-deadreckoning.sh
@@ -13,7 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec roslaunch --screen deadreckoning deadreckoning.launch
+dt-exec roslaunch --screen deadreckoning deadreckoning_node.launch
 
 
 # ----------------------------------------------------------------------------
