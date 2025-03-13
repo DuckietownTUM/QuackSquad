b99a27730e0ee546aa352b196f2197933d78896e
andreapellegrin <andreapellegrin@hotmail.it>
2025-03-13 18:41:16 +0100

    Vehicle detection test enviroment setup

--- dt-core/packages/lane_control/src/lane_controller_node.py
+++ dt-core/packages/lane_control/src/lane_controller_node.py
@@ -260,7 +260,7 @@ class LaneControllerNode(DTROS):
         self.controller.update_parameters(self.params)
 
     def on_shutdown(self):
-        rospy.sleep(1)
+        rospy.sleep(0.5)
         stop_msg = Twist2DStamped(v=0, omega=0.0)
         self.pub_car_cmd.publish(stop_msg)
 
